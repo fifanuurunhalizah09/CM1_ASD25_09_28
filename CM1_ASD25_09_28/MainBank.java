7bb995d93a0d4b6884b36e09f1601a79c51c93b2
sheiyyara <sheilandrazarawiba561@gmail.com>
2025-04-16 11:11:12 +0700

    Case 1 dan 2

--- CM1_ASD25_09_28/MainBank.java
+++ CM1_ASD25_09_28/MainBank.java
@@ -11,7 +11,7 @@ public class MainBank {
         Bank b4 = new Bank("16270525", "Maria", "Krabela", "082-267-223-0234", "maria@gmail.com");
         Bank b5 = new Bank("16101617", "Gery", "Fatimah", "083-683-416-8323", "gery@gery.com");
 
-        Transaksi[] dataTransaksi = {
+        Transaksi[] dataTransaksiArray = {
             new Transaksi("TR005", 500000, 200000, 700000, "2025-04-01", "Credit", b1),
             new Transaksi("TR004", 700000, 100000, 600000, "2025-04-02", "Debit", b2),
             new Transaksi("TR003", 300000, 150000, 450000, "2025-04-03", "Credit", b3),
@@ -19,7 +19,10 @@ public class MainBank {
             new Transaksi("TR001", 200000, 50000, 250000, "2025-04-05", "Credit", b2)
         };
 
-        for (Transaksi t : dataTransaksi) {
+        
+        ServiceTransaksi service = new ServiceTransaksi(10);
+        for (int i = 0; i < dataTransaksiArray.length; i++) {
+            service.tambah(dataTransaksiArray[i]);
         }
 
         int pilihan;
@@ -37,11 +40,11 @@ public class MainBank {
             switch (pilihan) {
                 case 1:
                     System.out.println("== Data Norek ==");
-
+                    service.tampilkanDataBankSaja();
                     break;
                 case 2:
                     System.out.println("== Data Transaksi ==");
-       
+                    service.tampilkanDataTransaksi();
                     break;
                 case 3:
                     System.out.print("Masukkan No HP yang dicari: ");
--- CM1_ASD25_09_28/ServiceTransaksi.java
+++ CM1_ASD25_09_28/ServiceTransaksi.java
@@ -1,3 +1,44 @@
 public class ServiceTransaksi {
-    
+    Transaksi[] daftarTransaksi;
+    int jumlahData;
+
+    public ServiceTransaksi(int kapasitas) {
+        daftarTransaksi = new Transaksi[kapasitas];
+        jumlahData = 0;
+    }
+
+    public void tambah(Transaksi transaksiBaru) {
+        if (jumlahData < daftarTransaksi.length) {
+            daftarTransaksi[jumlahData] = transaksiBaru;
+            jumlahData++;
+        } else {
+            System.out.println("Data transaksi penuh!");
+        }
+    }
+
+    public void tampilkanDataBankSaja() {
+        System.out.printf("%-15s %-10s %-15s %-20s %-25s\n",
+            "No Rekening", "Nama", "Nama Ibu", "No HP", "Email");
+        for (int i = 0; i < jumlahData; i++) {
+            Bank bank = daftarTransaksi[i].bankAcc;
+            System.out.printf("%-15s %-10s %-15s %-20s %-25s\n",
+                bank.noRekening, bank.nama, bank.namaIbu, bank.noHp, bank.email);
+        }
+    }
+
+    public void tampilkanDataTransaksi() {
+        System.out.printf("%-10s %-15s %-10s %-15s %-15s %-18s %-10s\n",
+            "Kode", "No Rekening", "Saldo", "Debit/Kredit", "Final Saldo", "Tanggal Transaksi", "Type");
+        for (int i = 0; i < jumlahData; i++) {
+            Transaksi transaksi = daftarTransaksi[i];
+            System.out.printf("%-10s %-15s %-10.1f %-15.1f %-15.1f %-18s %-10s\n",
+                transaksi.kodeTransaksi,
+                transaksi.bankAcc.noRekening,
+                transaksi.saldo,
+                transaksi.inOutSaldo,
+                transaksi.finalSaldo,
+                transaksi.tanggalTransaksi,
+                transaksi.type);
+        }
+    }
 }
