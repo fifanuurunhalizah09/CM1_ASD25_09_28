597160363e692a02bbba7c23b2325e3f3b2be2bd
sheiyyara <sheilandrazarawiba561@gmail.com>
2025-04-16 10:05:35 +0700

    Transaksi

---
+++ CM1_ASD25_09_28/MainBank.java
@@ -0,0 +1,5 @@
+public class MainBank {
+    public static void main(String[] args) {
+        
+    }
+}
--- CM1_ASD25_09_28/Transaksi.java
+++ CM1_ASD25_09_28/Transaksi.java
@@ -1,3 +1,29 @@
 public class Transaksi {
+    String kodeTransaksi;
+    double saldo;
+    double inOutSaldo;
+    double finalSaldo;
+    String tanggalTransaksi;
+    String type;
+    Bank bankAcc;
     
-}
+    Transaksi(String kodeTransaksi, double saldo, double inOutSaldo, double finalSaldo, String tanggalTransaksi, String type, Bank bankAcc){
+        this.kodeTransaksi = kodeTransaksi;
+        this.saldo = saldo;
+        this.inOutSaldo = inOutSaldo;
+        this.finalSaldo = finalSaldo;
+        this.tanggalTransaksi = tanggalTransaksi;
+        this.type = type;
+        this.bankAcc = bankAcc;
+    }
+    
+    void tampilDataTransaksi(){
+        System.out.println("Kode Transaksi        :" + kodeTransaksi);
+        System.out.println("Saldo                 :" + saldo);
+        System.out.println("inOutSaldo            :" + inOutSaldo);
+        System.out.println("Final Saldo           :" + finalSaldo);
+        System.out.println("Tanggal Transaksi     :" + tanggalTransaksi);
+        System.out.println("Type                  :" + type);
+            System.out.println("Bank Account      : " + bankAcc);
+        }
+    }
